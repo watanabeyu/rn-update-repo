2d4a5cdb742771f6f4e0900e674f1f7e37990369
watanabeyu <fight.ippatu5648@gmail.com>
2020-08-11 02:35:48 +0900

    update index file

--- index.tsx
+++ index.tsx
@@ -1,5 +1,7 @@
-import { AppRegistry } from 'react-native';
+import { registerRootComponent } from 'expo';
 import App from './App';
-import { name as appName } from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
+// It also ensures that whether you load the app in the Expo client or in a native build,
+// the environment is set up appropriately
+registerRootComponent(App);
