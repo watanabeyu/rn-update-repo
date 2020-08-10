11cd4c03caff59e5eaccf76abc01681e0c31056b
watanabeyu <fight.ippatu5648@gmail.com>
2020-08-11 03:43:59 +0900

    decided to take it all out for error

--- index.tsx
+++ index.tsx
@@ -1,7 +1,14 @@
+import { AppRegistry, Platform } from 'react-native';
 import { registerRootComponent } from 'expo';
+
 import App from './App';
+import { name as appName } from './app.json';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in the Expo client or in a native build,
 // the environment is set up appropriately
-registerRootComponent(App);
+if (Platform.OS === "ios") {
+  registerRootComponent(App);
+} else {
+  AppRegistry.registerComponent(appName, () => App);
+}
